e35510d20b23b7e085289b72684179e2d0062469
ClaudioCarige <ccarige@gmail.com>
2023-01-25 14:05:55 -0300

    First Commit

---
+++ src/main/java/com/claudiocarige/portfoliclaudio/PortfolioClaudioApplication.java
@@ -0,0 +1,13 @@
+package com.claudiocarige.portfoliclaudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioClaudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioClaudioApplication.class, args);
+	}
+
+}
