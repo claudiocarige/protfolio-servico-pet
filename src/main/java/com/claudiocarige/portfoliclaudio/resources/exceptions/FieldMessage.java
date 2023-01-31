5a58ecdee89974beefae38e68445b429ced5ccca
ClaudioCarige <ccarige@gmail.com>
2023-01-31 20:33:09 -0300

    Criação das classes de tratamento de exceções das validações de atributos

---
+++ src/main/java/com/claudiocarige/portfoliclaudio/resources/exceptions/FieldMessage.java
@@ -0,0 +1,38 @@
+package com.claudiocarige.portfoliclaudio.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String fieldname;
+	private String message;
+
+	public FieldMessage() {
+		super();
+	}
+
+	public FieldMessage(String fieldname, String message) {
+		super();
+		this.fieldname = fieldname;
+		this.message = message;
+	}
+
+	public String getFieldname() {
+		return fieldname;
+	}
+
+	public void setFieldname(String fieldname) {
+		this.fieldname = fieldname;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
