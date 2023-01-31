ccaacbafc46823237105866744784527a11b5d7c
ClaudioCarige <ccarige@gmail.com>
2023-01-31 20:35:43 -0300

    Criação do metodo de exceção validationErros

--- src/main/java/com/claudiocarige/portfoliclaudio/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.claudiocarige.portfoliclaudio.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,29 +13,35 @@ import com.claudiocarige.portfoliclaudio.services.exceptions.ObjectNotFoundExcep
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class ResourceExceptionHandler { 
-	
+public class ResourceExceptionHandler {
+
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, 
-				HttpServletRequest request){
-		StandardError erro = new StandardError(System.currentTimeMillis(), 
-												HttpStatus.NOT_FOUND.value(), 
-												"Object not found", ex.getMessage(), 
-												request.getRequestURI());
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
+			HttpServletRequest request) {
+		StandardError erro = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Object not found", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
-		
-	}
-	
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, 
-				HttpServletRequest request){
-		StandardError erro = new StandardError(System.currentTimeMillis(), 
-												HttpStatus.BAD_REQUEST.value(), 
-												"Violação de dados", ex.getMessage(), 
-												request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
-		
+
 	}
 
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
+		StandardError erro = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados", ex.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, 
+				HttpServletRequest request){
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Validation error", "Erro na validação dos campos", request.getRequestURI());
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			error.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
 }
- 
