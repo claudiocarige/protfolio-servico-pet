15f185c79c8660fdf908a4a25a036495e0a2ed4d
ClaudioCarige <ccarige@gmail.com>
2023-02-02 01:45:44 -0300

    Criada a class UserSpringSecurity e ajustado a regra de negocio da classe

---
+++ src/main/java/com/claudiocarige/portfoliclaudio/security/UserSS.java
@@ -0,0 +1,71 @@
+package com.claudiocarige.portfoliclaudio.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.claudiocarige.portfoliclaudio.domain.enums.Profile;
+
+public class UserSS implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String password;
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public UserSS(Integer id, String name, String password, Set<Profile> profile) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.authorities = profile.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao()))
+				.collect(Collectors.toSet());
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return name;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
