4f696d084d2b2ee46b9a3d8264d63a933b4eda06
Claudio Carige <ccarige@gmail.com>
2023-06-02 08:34:19 -0300

    EmployeeResource Class Refactoring

--- src/main/java/com/claudiocarige/portfoliclaudio/resources/EmployeeResource.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/resources/EmployeeResource.java
@@ -1,47 +1,38 @@
 package com.claudiocarige.portfoliclaudio.resources;
 
+import com.claudiocarige.portfoliclaudio.domain.Employee;
+import com.claudiocarige.portfoliclaudio.domain.dtos.EmployeeDTO;
+import com.claudiocarige.portfoliclaudio.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.claudiocarige.portfoliclaudio.domain.Employee;
-import com.claudiocarige.portfoliclaudio.domain.dtos.EmployeeDTO;
-import com.claudiocarige.portfoliclaudio.services.EmployeeService;
-
-import javax.validation.Valid;
-
 @RestController
 @RequestMapping(value = "/employees")
+@RequiredArgsConstructor
 public class EmployeeResource {
 
-	@Autowired
-	private EmployeeService service;
+	private final EmployeeService service;
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<EmployeeDTO> findById(@PathVariable Integer id) {
-		Employee obj = service.findById(id);
-		return ResponseEntity.ok().body(new EmployeeDTO(obj));
+		return ResponseEntity.ok().body(new EmployeeDTO(service.findById(id)));
 	}
 
 	@GetMapping
 	public ResponseEntity<List<EmployeeDTO>> findAll() {
-		List<Employee> list = service.findAll();
-		List<EmployeeDTO> listDTO = list.stream().map(obj -> new EmployeeDTO(obj)).
-									collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);
+		return ResponseEntity.ok()
+				.body(service.findAll()
+						.stream()
+						.map(EmployeeDTO::new)
+						.collect(Collectors.toList()));
 	}
 	
 	@PreAuthorize("hasAnyRole('ADMIN')")
@@ -57,8 +48,7 @@ public class EmployeeResource {
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<EmployeeDTO> update(@PathVariable Integer id, 
 												@Valid @RequestBody EmployeeDTO objDTO){
-		Employee obj = service.update(id, objDTO);
-		return ResponseEntity.ok().body(new EmployeeDTO(obj));
+		return ResponseEntity.ok().body(new EmployeeDTO(service.update(id, objDTO)));
 	}
 	
 	@PreAuthorize("hasAnyRole('ADMIN')")
