f8043b9ef5cb1d5c007c4af50d1cc37762dce8f9
Claudio Carige <ccarige@gmail.com>
2023-06-02 01:19:21 -0300

    Refactored EmployeeDTO Class

--- src/main/java/com/claudiocarige/portfoliclaudio/domain/dtos/EmployeeDTO.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/domain/dtos/EmployeeDTO.java
@@ -3,6 +3,8 @@ package com.claudiocarige.portfoliclaudio.domain.dtos;
 import com.claudiocarige.portfoliclaudio.domain.Employee;
 import com.claudiocarige.portfoliclaudio.domain.enums.Profile;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotNull;
@@ -12,6 +14,10 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class EmployeeDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -24,17 +30,17 @@ public class EmployeeDTO implements Serializable {
 	protected String cpf;
 	@NotNull(message = "O campo EMAIL é requerido!")
 	protected String email;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@NotNull(message = "O campo SENHA é requerido!")
 	protected String password;
+
+	@Getter(AccessLevel.NONE)
+	@Setter(AccessLevel.NONE)
 	protected Set<Integer> profile = new HashSet<>();
 
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate createDate = LocalDate.now();
-
-	public EmployeeDTO() {
-		super();
-		addProfile(Profile.EMPLOYEE);
-	}
 
 	public EmployeeDTO(Employee obj) {
 		super();
@@ -45,62 +51,14 @@ public class EmployeeDTO implements Serializable {
 		this.password = obj.getPassword();
 		this.profile = obj.getProfile().stream().map(Profile::getCodigo).collect(Collectors.toSet());
 		this.createDate = obj.getCreateDate();
-		addProfile(Profile.EMPLOYEE);
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public Set<Profile> getProfile() {
-		return profile.stream().map(x -> Profile.toEnum(x)).collect(Collectors.toSet());
+		return profile.stream().map(Profile::toEnum).collect(Collectors.toSet());
 	}
 
 	public void addProfile(Profile profile) {
 		this.profile.add(profile.getCodigo());
 	}
 
-	public LocalDate getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(LocalDate createDate) {
-		this.createDate = createDate;
-	}
 }
