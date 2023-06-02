16e0bf268706f41a07e5cc120e699fdfd7e0a211
Claudio Carige <ccarige@gmail.com>
2023-06-02 01:33:05 -0300

    Refactored ServicePetDTO Class

--- src/main/java/com/claudiocarige/portfoliclaudio/domain/dtos/ServicePetDTO.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/domain/dtos/ServicePetDTO.java
@@ -1,14 +1,20 @@
 package com.claudiocarige.portfoliclaudio.domain.dtos;
 
+import com.claudiocarige.portfoliclaudio.domain.ServicesPet;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import javax.validation.constraints.NotNull;
-
-import com.claudiocarige.portfoliclaudio.domain.ServicesPet;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class ServicePetDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -33,10 +39,6 @@ public class ServicePetDTO implements Serializable {
 	private String nameEmploye;
 	private String nameClient;
 
-	public ServicePetDTO() {
-		super();
-	}
-
 	public ServicePetDTO(ServicesPet obj) {
 		this.id = obj.getId();
 		this.openingDate = obj.getOpeningDate();
@@ -49,93 +51,5 @@ public class ServicePetDTO implements Serializable {
 		this.client = obj.getClient().getId();
 		this.nameEmploye = obj.getEmployee().getName();
 		this.nameClient = obj.getClient().getName();
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public LocalDate getOpeningDate() {
-		return openingDate;
-	}
-
-	public void setOpeningDate(LocalDate openingDate) {
-		this.openingDate = openingDate;
-	}
-
-	public LocalDate getClosingDate() {
-		return closingDate;
-	}
-
-	public void setClosingDate(LocalDate closingDate) {
-		this.closingDate = closingDate;
-	}
-
-	public Integer getPriority() {
-		return priority;
-	}
-
-	public void setPriority(Integer priority) {
-		this.priority = priority;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-
-	public Integer getEmployee() {
-		return employee;
-	}
-
-	public void setEmployee(Integer employee) {
-		this.employee = employee;
-	}
-
-	public Integer getClient() {
-		return client;
-	}
-
-	public void setClient(Integer client) {
-		this.client = client;
-	}
-
-	public String getNameEmploye() {
-		return nameEmploye;
-	}
-
-	public void setNameEmploye(String nameEmploye) {
-		this.nameEmploye = nameEmploye;
-	}
-
-	public String getNameClient() {
-		return nameClient;
-	}
-
-	public void setNameClient(String nameClient) {
-		this.nameClient = nameClient;
 	}
 }
