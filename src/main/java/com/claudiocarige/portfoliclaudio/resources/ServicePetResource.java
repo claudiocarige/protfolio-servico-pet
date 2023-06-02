7790de7f7165b6d86995a4ad829f6cefe0169c3f
Claudio Carige <ccarige@gmail.com>
2023-06-02 00:27:28 -0300

    Refactored ServicePetResource Class

--- src/main/java/com/claudiocarige/portfoliclaudio/resources/ServicePetResource.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/resources/ServicePetResource.java
@@ -1,25 +1,17 @@
 package com.claudiocarige.portfoliclaudio.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.claudiocarige.portfoliclaudio.domain.ServicesPet;
 import com.claudiocarige.portfoliclaudio.domain.dtos.ServicePetDTO;
 import com.claudiocarige.portfoliclaudio.services.ServicePetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/servicepet")
@@ -30,15 +22,16 @@ public class ServicePetResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ServicePetDTO> findById(@PathVariable Integer id) {
-		ServicesPet obj = service.findById(id);
-		return ResponseEntity.ok().body(new ServicePetDTO(obj));
+		return ResponseEntity.ok().body(new ServicePetDTO(service.findById(id)));
 	}
 
 	@GetMapping
 	public ResponseEntity<List<ServicePetDTO>> findAll() {
-		List<ServicesPet> list = service.finAll();
-		List<ServicePetDTO> listDTO = list.stream().map(obj -> new ServicePetDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);
+		return ResponseEntity.ok()
+				.body(service.finAll()
+						.stream()
+						.map(ServicePetDTO::new)
+						.collect(Collectors.toList()));
 	}
 
 	@PostMapping
@@ -50,7 +43,6 @@ public class ServicePetResource {
 
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ServicePetDTO> update(@PathVariable Integer id, @Valid @RequestBody ServicePetDTO objDTO) {
-		ServicesPet servicesPet = service.update(id, objDTO);
-		return ResponseEntity.ok().body(new ServicePetDTO(servicesPet));
+		return ResponseEntity.ok().body(new ServicePetDTO(service.update(id, objDTO)));
 	}
 }
