d71c81713a7fa8a8f6b6a99403d6f9f938e2bd1a
Claudio Carige <ccarige@gmail.com>
2023-06-02 08:41:00 -0300

    Refactored StandardError Class

--- src/main/java/com/claudiocarige/portfoliclaudio/resources/exceptions/StandardError.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/resources/exceptions/StandardError.java
@@ -1,7 +1,16 @@
 package com.claudiocarige.portfoliclaudio.resources.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StandardError implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -11,57 +20,4 @@ public class StandardError implements Serializable{
 	private String error;
 	private String message;
 	private String path;
-	
-	public StandardError() {
-		super();
-	}
-
-	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
-		super();
-		this.timestamp = timestamp;
-		this.status = status;
-		this.error = error;
-		this.message = message;
-		this.path = path;
-	}
-
-	public Long getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(Long timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-
-	public String getError() {
-		return error;
-	}
-
-	public void setError(String error) {
-		this.error = error;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
 }
