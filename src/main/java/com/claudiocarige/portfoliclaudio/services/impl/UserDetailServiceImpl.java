273af1f275788c42536cb5af1d14a40b0a8e3192
Claudio Carige <ccarige@gmail.com>
2023-06-11 18:11:47 -0300

    UserDatailServiceImpl in the correct folder

--- src/main/java/com/claudiocarige/portfoliclaudio/services/impl/UserDetailServiceImpl.java
+++ src/main/java/com/claudiocarige/portfoliclaudio/services/impl/UserDetailServiceImpl.java
@@ -1,16 +1,15 @@
-package com.claudiocarige.portfoliclaudio.services;
+package com.claudiocarige.portfoliclaudio.services.impl;
 
-import java.util.Optional;
-
+import com.claudiocarige.portfoliclaudio.domain.Person;
+import com.claudiocarige.portfoliclaudio.repositories.PersonRepository;
+import com.claudiocarige.portfoliclaudio.security.UserSS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.claudiocarige.portfoliclaudio.domain.Person;
-import com.claudiocarige.portfoliclaudio.repositories.PersonRepository;
-import com.claudiocarige.portfoliclaudio.security.UserSS;
+import java.util.Optional;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService{
